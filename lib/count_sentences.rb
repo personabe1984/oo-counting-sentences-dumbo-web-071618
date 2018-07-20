744ec5446f89d212a2cd8d9efe7b9ff57ae19be1
personabe1984 <personabe1984@gmail.com>
2018-07-20 09:14:24 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.split("").last == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    counter = 0
+    if !self
+      return counter
+    elsif
+      self.split(" ").each do |word|
+        if word.sentence? || word.question? || word.exclamation?
+          counter += 1
+        end
+      end
+    end
 
+    return counter         
   end
 end
